33a50a9b885274fac0ec75e518026127a67bd4d8
martin mithi <gathitumithi9000@gmail.com>
2022-08-08 17:53:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {
+  name: "Martin",
+  streetAddress: "110 Nairobi",
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return { ...obj, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const myObj = { ...employee };
+  delete myObj[key];
+  return myObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
